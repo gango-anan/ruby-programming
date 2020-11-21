d95bebe82f5ace002dd89000d951c44b9f9519e2
Galiwango Ananiya <galiwango101@gmail.com>
2020-11-21 22:13:41 +0300

    initial commit

---
+++ hello.rb
@@ -0,0 +1,38 @@
+puts "Hello, Gango"
+=begin
+Hello drawing a triangle
+ruby programming
+=end
+puts "    /|"
+puts "   / |"
+puts "  /  |"
+puts " /   |"
+puts "/____|"
+puts "AnAN" ; puts "Galiwango"
+y=3
+puts y.to_s(2)
+
+PI = 3.14
+r = 2.0
+area = PI*r*r
+puts area
+PI = 3.0
+r = 3.0
+area = PI*r*r
+puts area
+
+a,b,c,d,e = 1,2,3,4,5
+puts a
+puts b
+puts c
+puts d
+puts e
+
+puts a.kind_of? Float
+puts PI.class
+
+puts a.to_f
+puts c.to_s(2)
+puts 8.to_s(8)
+
+puts 10.to_s(16)
---
+++ reverseNumber.rb
@@ -0,0 +1,49 @@
+=begin
+num_array = Array.new
+puts "Enter 5 elements of the array, press enter after each element:"
+num_array[0] = gets.chomp().to_i
+num_array[1] = gets.chomp().to_i
+num_array[2] = gets.chomp().to_i
+num_array[3] = gets.chomp().to_i
+num_array[4] = gets.chomp().to_i
+puts "=============================="
+puts num_array
+
+=end
+puts "==================================="
+
+states = {
+"PA" => "Pennsylvania",
+"NY" => "New York",
+"IA" => "Iowa",
+"OR" => "Oregon"
+}
+
+puts states
+puts states["IA"]
+
+puts "=========================================================="
+
+def sayhi(name1, name2) 
+	return "Hi #{name1}" , "Hi #{name2}"
+end
+
+first_name = "Gango"
+second_name = "Anan"
+
+
+puts sayhi(first_name, second_name)
+
+puts ("======================================================")
+
+def max (num1, num2, num3)
+	if num1>=num2 and num1>=num3
+		return num1
+	elsif num2>=num1 and num2>=num3
+		return num2
+	else
+		return num3
+	end
+end
+
+puts max(1,5,2)
